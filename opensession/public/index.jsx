8aec4305887d35f36179bf8499fe3c1d18eb35f5
electric-otter <krw122081@gmail.com>
2024-12-27 15:32:06 -0800

    Update and rename index.html to index.jsx

---
+++ opensession/public/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function OpenSession() {
+  return (
+    <div>
+      <title>OpenSession</title>
+      <h1>VMs, storage, and even cloud services are so infuriating, cloud services making you pay, all to have a messy buggy computer-connecting space, not anymore! With OpenSession you have everything in a session.</h1>
+      {/* Add more content and features here */}
+    </div>
+  );
+}
+
+export default OpenSession;
